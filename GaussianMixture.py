7499e07b67072ea00bfd8103aa7d081d6ff03abb
Filippo Vicidomini <filippovicidomini@gmail.com>
2022-12-27 16:12:42 +0100

    qualcosa in gaussiano è stato fatto, da capire cosa...

--- GaussianMixture.py
+++ GaussianMixture.py
@@ -1,9 +1,3 @@
-# this is a file for the Gaussian Mixture Model (GMM) algorithm to classify the images of the dataset into 10 classes (0 to 9)
-# the GMM algorithm is a probabilistic model that assumes that the data is generated from a mixture of several Gaussian distributions
-# the GMM algorithm is a generative model
-# the GMM algorithm is a soft clustering algorithm
-# we assum thar the covariance matrix is diagonal
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -14,76 +8,34 @@ from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import train_test_split
 
 # open a csv file but do not consider the last 10 columns
-numbers = range(256)
-df = pd.read_csv('semeion.csv', sep=' ', usecols=range(0, 256), names=numbers)
-
-# this is the dataset that contains the right answer for each image in the dataset
-control = pd.read_csv('semeion.csv', sep=' ', usecols=range(256, 266), names=range(10))
-# i want to trasform control from a matrix of 0 and 1 to a vector of 0 to 9
-# for example, if the 10-dimensional vector is [0, 0, 0, 0, 0, 0, 0, 0, 0, 1], then the number is 9
-# if the 10-dimensional vector is [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], then the number is 8
-# if the 10-dimensional vector is [0, 0, 0, 0, 0, 0, 0, 1, 0, 0], then the number is 7
-
-control = np.array(control.idxmax(axis=1))
-
-# i want to see if the control dataset is correct
-# plt.imshow(df.iloc[1300].values.reshape(16, 16), cmap='gray')
-# plt.title('This is a ' + str(control[1300]))
-# plt.show()
-# the control dataset is correct
+df = pd.read_csv('semeion.csv', sep=' ', usecols=range(0, 256), names=range(0, 256))
 
 
-X_train, X_test, y_train, y_test = train_test_split(df, control, test_size=0.2, random_state=42)
+# from the result, we can see that n_components = 10 is the best choice
+GM: GaussianMixture = GaussianMixture(n_components=10, covariance_type='diag', random_state=0)
+GM.fit(df)
+y_pred = GM.predict(df)
+print(list(set(y_pred)))
+c0 = df[y_pred == 0]
+c1 = df[y_pred == 1]
+c2 = df[y_pred == 2]
+c3 = df[y_pred == 3]
+c4 = df[y_pred == 4]
+c5 = df[y_pred == 5]
+c6 = df[y_pred == 6]
+c7 = df[y_pred == 7]
+c8 = df[y_pred == 8]
+c9 = df[y_pred == 9]
+print(c0.shape, c1.shape, c2.shape, c3.shape, c4.shape, c5.shape, c6.shape, c7.shape, c8.shape, c9.shape)
 
-GBB = GaussianMixture(n_components=10, covariance_type='diag', max_iter=1000, random_state=0)
-
-GBB.fit(X_train, y_train)
-
-y_pred = GBB.predict(X_test)
-
-print('Accuracy: ', accuracy_score(y_test, y_pred))
-
-# i want to see the first imagin predicted by the GMM algorithm
-# the first image is a 16x16 matrix
-
-# plt.imshow(X_test.iloc[3].values.reshape(16, 16), cmap='gray', interpolation='nearest')
-# plt.axis('off')
-# plt.title('Predicted: {}'.format(y_pred[3]))
-# plt.show()
-
-# i want to see the confusion matrix using seaborn
-'''
-cm = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(10, 10))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-plt.title('Confusion matrix')
-plt.ylabel('Actual')
-plt.xlabel('Predicted')
+# plot the result
+fig, ax = plt.subplots(2, 5, figsize=(10, 5))
+for i in range(10):
+    ax[i // 5, i % 5].imshow(eval('c' + str(i)).mean(axis=0).values.reshape(16, 16), cmap='gray')
+    ax[i // 5, i % 5].set_title('cluster ' + str(i))
+    ax[i // 5, i % 5].axis('off')
 plt.tight_layout()
 plt.show()
-'''
-# now to improve perfomance a wanto to use PCA to reduce the dimensionality of the dataset
 
-from sklearn.decomposition import PCA
 
-pca = PCA(n_components=50)
-X_train_pca = pca.fit_transform(X_train)
-X_test_pca = pca.transform(X_test)
 
-GBB = GaussianMixture(n_components=10, covariance_type='diag', max_iter=1000, random_state=0)
-
-GBB.fit(X_train_pca, y_train)
-
-y_pred = GBB.predict(X_test_pca)
-
-print('Accuracy: ', accuracy_score(y_test, y_pred))
-# and see the confusion matrix again
-
-cm = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(10, 10))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-plt.title('Confusion matrix')
-plt.ylabel('Actual')
-plt.xlabel('Predicted')
-plt.tight_layout()
-plt.show()
--- MeanShift.py
+++ MeanShift.py
@@ -11,28 +11,12 @@ from sklearn.model_selection import train_test_split
 # load the data
 numbers = range(256)
 df = pd.read_csv('semeion.csv', sep=' ', usecols=range(0, 256), names=numbers)
-# print(df.shape)
 
-# this is the dataset that contains the right answer for each image in the dataset
-control = pd.read_csv('semeion.csv', sep=' ', usecols=range(256, 266), names=range(10))
-# print(control.head())
+bandwidth = estimate_bandwidth(df, quantile=0.2, n_samples=500)
 
-control = np.array(control.idxmax(axis=1))
-print(control)
+ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+ms.fit(df)
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
 
-X_train, X_test, y_train, y_test = train_test_split(df, control, test_size=0.5, random_state=7)
-
-mean_shift = MeanShift(bandwidth=10, bin_seeding=True)
-mean_shift.fit(X_train)
-y_pred = mean_shift.predict(X_test)
-
-# see the confusion matrix using seaborn
-
-mat = confusion_matrix(y_test, y_pred)
-sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
-            xticklabels=range(10),
-            yticklabels=range(10))
-plt.xlabel('true label')
-plt.ylabel('predicted label')
-plt.tight_layout()
-plt.show()
+print("number of estimated clusters : %d" % len(cluster_centers))
