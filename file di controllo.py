16e7de0926ebe64548e223f415a49d0d6fbabe73
Filippo Vicidomini <filippovicidomini@gmail.com>
2022-12-27 12:23:10 +0100

    as we expected il randomforest ha performance strepitose.

--- file di controllo.py
+++ file di controllo.py
@@ -1,13 +1,13 @@
-# the same clustering but using normlized cut
+# i want to see if a random forest can do better
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import seaborn as sns
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.cluster import SpectralClustering
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # open a csv file but do not consider the last 10 columns
 numbers = range(256)
@@ -18,27 +18,20 @@ control = pd.read_csv('semeion.csv', sep=' ', usecols=range(256, 266), names=ran
 # i want to trasform control from a matrix of 0 and 1 to a vector of 0 to 9
 control = np.array(control.idxmax(axis=1))
 
-# i want to see if the control dataset is correct
-# plt.imshow(df.iloc[1300].values.reshape(16, 16), cmap='gray')
-# plt.title('This is a ' + str(control[1300]))
-# plt.show()
-# the control dataset is correct
+rfc = RandomForestClassifier(n_estimators=100, random_state=42)
 
 X_train, X_test, y_train, y_test = train_test_split(df, control, test_size=0.2, random_state=42)
 
-normalized_cut = SpectralClustering(n_clusters=10, affinity='nearest_neighbors', random_state=0)
+rfc.fit(X_train, y_train)
 
-normalized_cut.fit(X_train)
-
-y_pred = normalized_cut.fit_predict(X_test)
+y_pred = rfc.predict(X_test)
 
 # see the confusion matrix using seaborn
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(10, 10))
-sns.heatmap(cm, annot=True, fmt='d', cmap=sns.color_palette("rocket", as_cmap=True))
+sns.heatmap(cm, annot=True, fmt='d', cmap=sns.color_palette("ch:start=.2,rot=-.3", as_cmap=True))
 plt.title('Confusion matrix \n Accuracy: {}'.format(accuracy_score(y_test, y_pred)))
 plt.ylabel('Actual')
 plt.xlabel('Predicted')
 plt.tight_layout()
 plt.show()
-
