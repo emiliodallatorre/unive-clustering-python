1e36d93a4b4977ab0deb6af27b4146ee943ecda4
Filippo Vicidomini <filippovicidomini@gmail.com>
2022-12-30 19:14:20 +0100

    non ho usato bayesian

--- BayesianGaussianMixture.py
+++ BayesianGaussianMixture.py
@@ -2,7 +2,7 @@ import time as time
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
-from sklearn.mixture import BayesianGaussianMixture
+from sklearn.mixture import BayesianGaussianMixture, GaussianMixture
 from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('semeion.csv', sep=' ', usecols=range(0, 256), names=range(0, 256))
@@ -16,7 +16,7 @@ for i in range(10):
         pca = PCA(n_components=n_pca[i])
         X_pca = pca.fit_transform(X_std)
         start = time.time()
-        bgm = BayesianGaussianMixture(n_components=n_pca[j], covariance_type='diag', max_iter=1000, random_state=0)
+        bgm = GaussianMixture(n_components=n_pca[j], covariance_type='diag', max_iter=1000, random_state=0)
         bgm.fit(X_pca)
         ax[i][j].scatter(X_pca[:, 0], X_pca[:, 1], c=bgm.predict(X_pca), s=40, cmap='nipy_spectral')
         ax[i][j].set_title(
