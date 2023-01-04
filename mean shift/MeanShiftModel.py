981c4878291752695e0cc787cd6405aa4ff14754
Filippo Vicidomini <filippovicidomini@gmail.com>
2023-01-04 23:18:33 +0100

    da rivedere la disposizione del rand index

--- mean shift/MeanShiftModel.py
+++ mean shift/MeanShiftModel.py
@@ -51,7 +51,6 @@ for i in range(8):
             ax[j, i].text(0.01, 0.01, f'clusters = {n_clusters_}',
                           transform=ax[j, i].transAxes, size=15,
                           horizontalalignment='left')
-
             # calcolo rand index
             rand_indexx = rand_index_(control, labels)
             # want to add the rand index to every subplot in corner top right
