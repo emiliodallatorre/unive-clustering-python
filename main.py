fe438dcadf665ee8196b42a1a8c4998978d7effa
Emilio Dalla Torre <info@emiliodallatorre.it>
2023-05-30 10:09:18 +0200

    Implementato il plot del GaussianMixtureModel.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame, read_csv, Series
 
 from clustering_model_interface import ClusteringModelInterface
-from models.gaussian_mixture_model import GaussianMixtureModel
+from plots.plot_gaussian_mixture import PlotGaussianMixture
 
 data: DataFrame = read_csv('data/semeion.csv', sep=' ', usecols=range(256), names=range(256))
 control: DataFrame = read_csv('data/semeion.csv', sep=' ', usecols=range(256, 266), names=range(10))
@@ -10,8 +10,15 @@ control: Series = control.idxmax(axis=1)
 models: list[ClusteringModelInterface] = [
     # NormalizedCutModel(data, control, [2, 3, 4, 5, 6]),
     # MeanShiftModel(data, control, [2, 3, 4, 5, 6, 8, 10, 20]),
-    GaussianMixtureModel(data, control, [3, 10, 15, 20, 25, 50, 256]),
+    # GaussianMixtureModel(data, control, [3, 10, 15, 20, 25, 50, 256]),
+]
+
+plots: list = [
+    PlotGaussianMixture(data, control),
 ]
 
 for model in models:
     model.perform_clustering()
+
+for plot in plots:
+    plot.plot()
---
+++ plots/plot_gaussian_mixture.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+from pandas import DataFrame, Series
+from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
+from sklearn.preprocessing import StandardScaler
+
+
+class PlotGaussianMixture:
+    images_out_path: str = "images";
+
+    data: DataFrame = None
+    control: Series = None
+    standardized_x: DataFrame = None
+
+    def __init__(self, data: DataFrame, control: Series) -> None:
+        self.data = data
+        self.control = control
+
+        self.standardized_x = StandardScaler().fit_transform(data)
+
+    def plot(self):
+        pca = PCA(n_components=25)
+        x_pca = pca.fit_transform(self.standardized_x)
+
+        model: GaussianMixture = GaussianMixture(n_components=12, covariance_type='diag', random_state=1)
+        model.fit(x_pca)
+        predicted_y = model.predict(x_pca)
+
+        reconstructed_x = pca.inverse_transform(x_pca)
+
+        fig, ax = plt.subplots(3, 5, figsize=(15, 11))
+        for i in range(15):
+            ax[i // 5, i % 5].imshow(reconstructed_x[predicted_y == i].mean(axis=0).reshape(16, 16), cmap='Blues')
+            ax[i // 5, i % 5].set_title('pca: ' + str(pca.n_components_) + '\ncluster n: ' + str(i), fontsize=15)
+            ax[i // 5, i % 5].axis('off')
+
+        plt.tight_layout()
+        # plt.show()
+        plt.savefig(f'{self.images_out_path}/{self.get_plot_name()}.png')
+
+    def get_plot_name(self) -> str:
+        return self.__class__.__name__
